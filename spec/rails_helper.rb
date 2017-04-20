2fb66dd66ed31243d47521530961f82cee7c1be1
Corey Gibson <cgibsonmm@gmail.com>
2017-04-20 10:41:15 -0400

    fixed typos to helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,8 +26,9 @@ require 'rspec/rails'
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
-Shoulda::Matcher.configure do |config|
-  config.intergrate do |with|
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
     with.test_framework :rspec
     with.library :rails
   end
@@ -46,7 +47,7 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
-    DatabaseCleaner.stratagy = :transaction
+    DatabaseCleaner.strategy = :transaction
   end
 
   config.around(:each) do |example|
